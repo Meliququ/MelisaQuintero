44801a98484e90f8ca9456218510f9ad7e6ea66a
Meliququ <melisaquintero229598@correo.itm.edu.co>
2021-08-23 11:56:16 -0500

    Update Todo

---
+++ Todoquintero.Function/Functions/TodoApi.cs
@@ -0,0 +1,120 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Todoquintero.Common;
+using Todoquintero.Common.Responses;
+using Todoquintero.Function.Entities;
+
+namespace Todoquintero.Function.Functions
+{
+    public static class TodoApi
+    {
+        [FunctionName(nameof(CreateTodo))]
+        public static async Task<IActionResult> CreateTodo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "todo")] HttpRequest req,
+            [Table("todo", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+            ILogger log)
+        {
+            log.LogInformation("Recieved a new todo.");
+
+
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            todo todo = JsonConvert.DeserializeObject<todo>(requestBody);
+
+            if (string.IsNullOrEmpty(todo?.TaskDescription))
+            {
+                return new BadRequestObjectResult(new Responses
+                {
+                    IsSuccess = false,
+                    Message = "The request must have a TaskDescription."
+
+                });
+            }
+
+            TodoEntity todoEntity = new TodoEntity
+            {
+                createdtime = DateTime.UtcNow,
+                ETag = "*",
+                Iscompleted = false,
+                PartitionKey = "TODO",
+                RowKey = Guid.NewGuid().ToString(),
+                TaskDescription = todo.TaskDescription
+            };
+
+            TableOperation addOperation = TableOperation.Insert(todoEntity);
+            await todoTable.ExecuteAsync(addOperation);
+
+            string message = "New todo stored in table";
+            log.LogInformation(message);
+
+
+
+            return new OkObjectResult(new Responses
+            {
+                IsSuccess = true,
+                Message = message,
+                Result = todoEntity
+            });
+        }
+
+
+
+        [FunctionName(nameof(UpdateTodo))]
+        public static async Task<IActionResult> UpdateTodo(
+       [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "todo/{id}")] HttpRequest req,
+       [Table("todo", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+       string id,
+       ILogger log)
+        {
+            log.LogInformation($"Update for todo: {id},received.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            todo todo = JsonConvert.DeserializeObject<todo>(requestBody);
+            //validate todo id
+            TableOperation findOperation = TableOperation.Retrieve<TodoEntity>("TODO", id);
+            TableResult findResult = await todoTable.ExecuteAsync(findOperation);
+
+            if (findResult.Result == null)
+            {
+                return new BadRequestObjectResult(new Responses
+                {
+                    IsSuccess = false,
+                    Message = "Todo not found."
+
+                });
+            }
+
+            //Update todo
+            TodoEntity todoEntity = (TodoEntity)findResult.Result;
+            todoEntity.Iscompleted = todo.Iscompleted;
+            if (!string.IsNullOrEmpty(todo.TaskDescription))
+            {
+                todoEntity.TaskDescription = todo.TaskDescription;
+            }
+
+
+            TableOperation replaceOperation = TableOperation.Replace(todoEntity);
+            await todoTable.ExecuteAsync(replaceOperation);
+
+            string message = $"Todo: {id} update in table.";
+            log.LogInformation(message);
+
+
+
+            return new OkObjectResult(new Responses
+            {
+                IsSuccess = true,
+                Message = message,
+                Result = todoEntity
+            });
+        }
+    }
+}
